4d5ff0185e82edb563aef65171758775df9be9ee
SEAN GILDEA <seanjgildea@gmail.com>
2019-06-21 09:10:52 -0400

    file upload v1 complete

--- src/main/java/com/example/easynotes/controller/UploadController.java
+++ src/main/java/com/example/easynotes/controller/UploadController.java
@@ -5,19 +5,27 @@ import com.example.easynotes.model.Game;
 import com.example.easynotes.model.Upload;
 import com.example.easynotes.repository.GameRepository;
 import com.example.easynotes.repository.UploadRepository;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.util.logging.Logger;
 
 @Controller()
-public class UploadController {
+public class UploadController implements HandlerExceptionResolver {
 
     private static final Logger log = Logger.getLogger(UploadController.class.getName());
 
@@ -34,11 +42,17 @@ public class UploadController {
     }
 
     @PostMapping("/file/upload")
-    public String upload(@RequestParam("file") MultipartFile file, ModelMap modelMap) {
+    public String upload(@RequestParam("file") MultipartFile file,
+                         @RequestParam String name,
+                         @RequestParam String email,
+                         ModelMap modelMap) {
 
         log.info("INSIDE UPLOAD FILE METHOD");
 
         Upload upload = new Upload();
+
+        upload.setEmail(email);
+        upload.setName(name);
 
         try {
             upload.setFile(file.getBytes());
@@ -49,9 +63,45 @@ public class UploadController {
 
         uploadRepository.save(upload);
 
-
         modelMap.addAttribute("games", gameRepository.findAllByOrderByCreateDateDesc());
         return "viewGames";
+    }
+
+
+    @GetMapping("/file/view")
+    public String viewFiles(Model model) {
+
+        model.addAttribute("uploads", uploadRepository.findAll());
+        return "viewFiles";
+    }
+
+
+
+    @Transactional
+    @RequestMapping(value = "/imageDisplay", method = RequestMethod.GET)
+    public void showImage(@RequestParam("id") Long uploadId, HttpServletResponse response, HttpServletRequest request)
+            throws ServletException, IOException {
+
+        Upload upload = uploadRepository.getOne(uploadId);
+
+        response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+        response.getOutputStream().write(upload.getFile());
+
+        response.getOutputStream().close();
+    }
+
+    @Override
+    public ModelAndView resolveException(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object object,
+            Exception exc) {
+
+        ModelAndView modelAndView = new ModelAndView("fileUpload");
+        if (exc instanceof MaxUploadSizeExceededException) {
+            modelAndView.getModel().put("message", "File size exceeds limit!");
+        }
+        return modelAndView;
     }
 
 }
